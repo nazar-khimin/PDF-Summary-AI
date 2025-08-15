e7072a0ee57874f72bcd728c681bdc852c14f2a5
nazar.khimin <nazar.khimin@unanet.com>
2025-08-15 16:13:42 +0300

    variable renaming

--- src/app.py
+++ src/app.py
@@ -1,21 +1,20 @@
 import streamlit as st
 from dotenv import load_dotenv
 from pdf_loader import load_pdf
-from openai_clients import get_gpt_4o_mini_llm, get_gpt5_mini_llm
 from summarizer import summarize_pdf
 
 load_dotenv()
 st.set_page_config(page_title="📄 PDF Summarizer", layout="centered")
 st.title("📄 PDF Summarizer")
 
-uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+uploaded_pdf = st.file_uploader("Upload a PDF file", type="pdf")
 
-if uploaded_file:
-    docs = load_pdf(uploaded_file)
+if uploaded_pdf:
+    pdf = load_pdf(uploaded_pdf)
     if st.button("Summarize"):
         with st.spinner("Summarizing..."):
             try:
-                summary = summarize_pdf(docs)
+                summary = summarize_pdf(pdf)
                 st.subheader("Summary:")
                 st.markdown(summary)
             except Exception as e:
