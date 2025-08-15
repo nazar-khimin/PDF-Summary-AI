3760068eeb73990067ff97172a09070a018dfd23
nazar.khimin <nazar.khimin@unanet.com>
2025-08-15 16:55:20 +0300

    Added better models

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,6 +1,6 @@
 from langchain.chains.summarize import load_summarize_chain
 
-from openai_clients import get_gpt5_mini_llm
+from openai_clients import get_gpt_4_1
 
 
 def summarize_pdf(docs):
@@ -8,7 +8,7 @@ def summarize_pdf(docs):
         return "No documents to summarize."
 
     chain = load_summarize_chain(
-        llm= get_gpt5_mini_llm(),
+        llm= get_gpt_4_1(),
         chain_type="map_reduce"
     )
     return chain.invoke({"input_documents": docs})["output_text"]
